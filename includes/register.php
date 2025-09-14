cdd4b7fba09ccea233449ac4d5aec3a9ea2351b0
= <=>
2025-09-14 15:22:11 +0700

    web-travel

--- includes/register.php
+++ includes/register.php
@@ -1,3 +1,13 @@
 <?php
+$con = mysqli_connect("localhost","root","","dbs_travel") or die("Connection was not established");
 
+$usernmae = "";
+$password = "";
+
+if(isset($_GET['act']) && $_GET['act'] == 'hapus' && isset($_GET['id'])){
+    $usernmae_hapus = $_GET['id'];
+    mysqli_query($con, "DELETE FROM users WHERE id = '$usernmae_hapus'");
+    header('location:?');
+    exit;
+}
 ?>
